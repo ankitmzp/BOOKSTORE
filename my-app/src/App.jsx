e9767de757bdea37cfc7fb9fb36b8647e07592d3
ankitmzp <amgood336@gmail.com>
2024-05-03 10:06:33 +0530

    final project done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import BookStore from "./BookStore";
+
+const App = () => (
+  <>
+  
+  <h1 style ={{textAlign: "center"}}>Best Seller Book </h1>
+
+
+
+<BookStore />;
+</>
+)
+
+export default App;
